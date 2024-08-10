73b4af5fbc52261dc5003c3ce42bccba1c6e8823
lleull <leulmekonnen049@gmail.com>
2024-08-10 09:37:34 +0300

    tailwindConfi, utils, shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl underline">Careplus</h1>
+      <h1 className="text-3xl underline">Careplus</h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
